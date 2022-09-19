023ea1240e2bddfed3a1d62e7f791cf86b88512d
VeziNjabulo <vezinjabulo@hotmail.com>
2022-09-19 12:37:21 -0700

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{ 
+	write(STDOUT_FILENO, "Aand that piece of art is useful
+			\" - Dora Korpa, 2015-10-19\n" 59);
+	return (1);
+}
