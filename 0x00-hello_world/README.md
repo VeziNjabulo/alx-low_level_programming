93c59c5c6c6d5b5833d521c271cfe4becb56b014
VeziNjabulo <vezinjabulo@hotmail.com>
2022-09-20 20:27:39 +0200

    submithm

--- 0x00-hello_world/README.md
+++
@@ -1,4 +0,0 @@
-0x00. C - Hello, World
-
-1.0. Preprocessor:a script that runs a C file through the preprocessor and save the result into another file
-
--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-/**
- * main -Determine if a random numbre is positive, negetive or zero.
-(* 
- * Return: Always 0 (Success)
+/*
+ * File: 0-positive_or_negative.c
  */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n < 0)
-	{
-		printf("%d is %s\n", n, "negetive");
-	}
-        else if (n > 0)
-	{
-		printf("%d is %s\n", n, "positive");
-	}
-        else
-	{	
-		printf("%d is %s\n", n, "zero");
-	}
-	return (0);
 
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
 }
