43d1cb0d8f717be721e8f8de009a93e8cce3d281
VeziNjabulo <vezinjabulo@hotmail.com>
2022-09-21 10:09:32 +0200

    submit 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,10 @@
 
 #include "main.h"
 
+/**
+ * _islower - Checks if a character is lowercase.
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,6 +4,12 @@
 
 #include "main.h"
 
+/*
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
 int _abs(int n)
 {
 	if (n >= 0)
