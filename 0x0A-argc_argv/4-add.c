c3fc41f8928ad08f4fd5b998adaad18fd0c7444a
VeziNjabulo <vezinjabulo@hotmail.com>
2022-10-23 11:31:27 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,33 +11,34 @@
 *@argv: array of arguments
 *Return: 0 on success, 1 if error
 */
-
 int main(int argc, char *argv[])
 {
-	int i;
-	unsigned int j;
-	int sum = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
-	{
+	if (argc < 2)
 		printf("0\n");
-		return (0);
-	}
 	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
-			for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				if (isdigit(argv[i][j]) == 0)
+				if (isdigit(*(ptr + j)) == 0)
 				{
 					printf("Error\n");
 					return (1);
 				}
 			}
+
 			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
+
+	printf("%d\n", sum);
 	}
 	return (0);
 }
